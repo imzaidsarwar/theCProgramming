9b3b1304af660f15c27f5ce2e4f393ac042e6f67
sarwar0001 <140310771+imzaidsarwar@users.noreply.github.com>
2024-03-17 15:32:05 +0530

    Complete the exercise 13 and push the exe file of exercise 16 and 19

--- chapter1/exercise13.c
+++ chapter1/exercise13.c
@@ -4,3 +4,35 @@
     It is easy to draw the histogram with the bars horizontal; a vertical
     orientation is more challenging.
 */
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    /* Store the input from getchar() */
+    int inputCharacter;
+    /* Represent the length of histogram */
+    int len =0;
+    while((inputCharacter = getchar()) != EOF)
+    {
+        if(inputCharacter != ' ' && inputCharacter != '\n')
+        {   
+            /* Increment the length of histogram */
+            len++;
+        }
+        else
+        {
+            /* Print the length of histogram */
+            printf("%d |",len);
+            /* Print the histogram */
+            for(int i=0;i<=len-1;i++)
+            {
+                putchar('X');
+            }
+            /* Reset the length from 0 for another histogram */
+            len = 0;
+            /* Print new line for another histogram */
+            printf("\n");
+        }
+    }
+    return 0;
+}
